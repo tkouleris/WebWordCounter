9d248d6dfc7623ede09dd9e7d7d05aa7de3c97c5
darkheart101 <kurokokoro@windowslive.com>
2020-11-03 06:50:47 +0200

    feature: get words from html page

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,10 +5,31 @@ use Symfony\Component\DomCrawler\Crawler;
 
 Route::get('wordcounter',function (){
     $html = file_get_contents('http://tkouleris.eu/');
-    $crawler = new Crawler($html);
 
+    $crawler = new Crawler($html);
+    $crawler->filter('script')->each(function (Crawler $crawler) {
+        foreach ($crawler as $node) {
+            $node->parentNode->removeChild($node);
+        }
+    });
+    $text = "";
     foreach ($crawler as $domElement) {
-        var_dump($domElement->nodeName);
+        $text = $domElement->nodeValue;
     }
+
+    $new_array = explode(" ",$text);
+
+    $clean_up_array = [];
+    foreach ($new_array as $item)
+    {
+        $item = trim(preg_replace('/\s\s+/', ' ', $item));
+        $item = preg_replace('/[^A-Za-z0-9\-]/', '', $item);
+        if($item === "") continue;
+        if($item === "-") continue;
+
+        $clean_up_array[] = $item;
+
+    }
+    dd($clean_up_array);
     return 'word counter';
 });
